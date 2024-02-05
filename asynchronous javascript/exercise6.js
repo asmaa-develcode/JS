8c88a1efcadb1174d48f11ac60f6b51f7ee73d98
asmaa-develcode <asmaaacademicmatters@gmail.com>
2024-02-05 16:32:23 +0100

    c

---
+++ asynchronous javascript/exercise6.js
@@ -0,0 +1,33 @@
+const isLogged = true;
+
+const firstPromise= new Promise((resolve,reject)=>{
+    if(isLogged){
+        resolve(Math.random()); 
+    }
+    else{
+        reject("Error");
+    }
+}
+)
+
+const secondPromise=(num)=>{
+    new Promise=((resolve,reject)=>{
+        if(num>0.5){
+            resolve({name: "John", age: 24});
+        } else{
+            reject("Error");
+        }
+    }
+    )
+}
+
+firstPromise
+.then((randomNumber)=>secondPromise(randomNumber))
+.then((finalObject)=>{
+    console.log({name: "John", age: 24});
+}
+)
+.catch((error)=>{
+    console.error(error);
+}
+);
